17c4f688002924f12b653e6030acc71ae54287a4
RWM <rwmingad@163.com>
2018-12-21 16:08:33 +0800

    QuickSort

---
+++ src/sorting/advance/QuickSort.java
@@ -0,0 +1,62 @@
+package sorting.advance;
+
+import sorting.SortHelper;
+
+/**
+ * @author RWM
+ * @date 2018/12/21
+ */
+public class QuickSort {
+
+    private QuickSort() {
+    }
+
+    public static void sort(Comparable[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        sort(arr, left, right);
+    }
+
+    private static void sort(Comparable[] arr, int left, int right) {
+        if (left >= right) return;
+        int j = partition(arr, left, right);
+        sort(arr, left, j - 1);
+        sort(arr, j + 1, right);
+    }
+
+    private static int partition(Comparable[] arr, int left, int right) {
+        int i = left;
+        int j = right + 1;
+
+        Comparable v = arr[left];
+        while (true) {
+            while (SortHelper.less(arr[++i], v)) {
+                if (i >= right) break;
+            }
+
+            while (SortHelper.less(v, arr[--j])) {
+                if (j <= left) break;
+            }
+
+            if (i >= j) {
+                break;
+            }
+
+            swap(arr, i, j);
+        }
+
+        swap(arr, left, j);
+        return j;
+    }
+
+    private static void swap(Object[] arr, int i, int j) {
+        Object temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = SortHelper.generateRandomArray(20, 0, 20);
+        sort(arr);
+    }
+}
