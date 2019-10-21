3d486fc31d5c4fde410357bf806614e4b4617223
weimin_ruan <rwmingad@163.com>
2019-10-21 21:21:12 +0800

    二叉树的深度

--- src/offer/二叉树/TreeDepth.java
+++ src/offer/二叉树/TreeDepth.java
@@ -20,4 +20,15 @@ public class TreeDepth {
         return Math.max(leftDepth + 1, rightDepth + 1);
     }
 
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode left = new TreeNode(2);
+        TreeNode right = new TreeNode(4);
+        TreeNode leftLeft = new TreeNode(3);
+        root.left = left;
+        root.right = right;
+        left.left = leftLeft;
+        System.out.println(treeDepth(root));
+    }
+
 }
