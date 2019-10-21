71b904c71c7225cf029f6180da94de9c2474e1d1
weimin_ruan <rwmingad@163.com>
2019-10-21 21:49:48 +0800

    二叉树的镜像

---
+++ src/offer/二叉树/Mirror.java
@@ -0,0 +1,21 @@
+package offer.二叉树;
+
+/**
+ * 二叉树的镜像
+ *
+ * @author RWM
+ * @date 2019/10/21
+ */
+public class Mirror {
+
+    public static void mirror(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        mirror(node.left);
+        mirror(node.right);
+    }
+}
