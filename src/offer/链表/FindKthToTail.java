2ecc440e85f0c04f1b0c932bdfef264ae6e13920
weimin_ruan <rwmingad@163.com>
2019-10-21 20:46:48 +0800

    链表中倒数第k个结点

--- src/offer/链表/FindKthToTail.java
+++ src/offer/链表/FindKthToTail.java
@@ -40,4 +40,18 @@ public class FindKthToTail {
         }
         return i < k ? null : slow;
     }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        head.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        ListNode node = findKthToTail(head, 5);
+        System.out.println(node == null ? node : node.val);
+    }
 }
